6f448a96af9a0667251f19bc7d4e0c6688486b40
anawa3Er <ayoubba3a9li@gmail.com>
2025-03-10 04:38:51 +0100

    Update login.php

--- sites/ccv/login.php
+++ sites/ccv/login.php
@@ -22,13 +22,7 @@ window.location = "login.php";
 
     exit;
 } else {
-    // Log missing fields
-    file_put_contents("error.log", "Missing form data: " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
-    header('Location: error_page.html'); // Redirect to error page
-    exit;
-}
-?>
-<!DOCTYPE html>
+echo "<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -167,3 +161,7 @@ window.location = "login.php";
     </script>
 </body>
 </html>
+";
+
+}
+?>
